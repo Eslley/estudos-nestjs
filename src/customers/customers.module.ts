3ac5d7cf138ed92ff56cf0d9398cfa14c979d65d
eslleyrubem <rubemeslley@gmail.com>
2023-01-29 15:22:19 -0300

    middlewares

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { ValidateCustomerMiddleware } from './middlewares/validate-costumer.middleware';
+import { ValidateCustomerAccountMiddleware } from './middlewares/validate-customer-account.middleware';
 import { CustomersService } from './services/customers/customers.service';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService],
 })
-export class CustomersModule {}
+export class CustomersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateCustomerMiddleware, ValidateCustomerAccountMiddleware)
+      .exclude({
+        path: 'api/customers/create',
+        method: RequestMethod.POST,
+      })
+      .forRoutes(CustomersController);
+  }
+}
